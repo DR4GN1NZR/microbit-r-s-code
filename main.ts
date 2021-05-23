c859703788ec720c59cf54abc641e2e335cf727d
DR4GN1NZR <84656811+DR4GN1NZR@users.noreply.github.com>
2021-05-23 09:13:40 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,23 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    p1 += -2
+    if (p1 == 0) {
+        p1 = 10
+        basic.showString("P1 lost")
+    }
 })
+input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(p1)
+    basic.showString("&")
+    basic.showNumber(p2)
+})
+input.onButtonPressed(Button.B, function () {
+    p2 += -2
+    if (p2 == 0) {
+        p2 = 10
+        basic.showString("P2 lost")
+    }
+})
+let p1 = 0
+let p2 = 0
+p2 = 10
+p1 = 10
